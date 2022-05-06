10cfde5be1abe36de84830bdfc7e09826a6e028e
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2022-05-06 10:49:41 +0700

    Revert "update require WC when active"

--- printcart-design.php
+++ printcart-design.php
@@ -19,12 +19,3 @@ define( 'PRINTCART_PLUGIN_URL',             plugin_dir_url( __FILE__ ) );
 define( 'PRINTCART_PLUGIN_DIR',             plugin_dir_path( __FILE__ ) );
 require_once( PRINTCART_PLUGIN_DIR . 'includes/class-admin-options.php' );
 require_once( PRINTCART_PLUGIN_DIR . 'vendor/autoload.php' );
-
-register_activation_hook( __FILE__, 'plugin_activation' );
-
-function plugin_activation( $network_wide ) {
-    if ( !is_plugin_active( 'woocommerce/woocommerce.php' ) ) {
-        $message = '<div class="error"><p>' . sprintf( __('WooCommerce is not active. Please activate WooCommerce before using %s.', 'web-to-print-online-designer' ), '<b>Printcart Integration</b>' ) . '</p></div>';
-        die( $message );
-    }
-}
