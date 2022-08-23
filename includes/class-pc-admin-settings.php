681e8eb4f029f944c83d5e3fa54a3c4445cdb4a8
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2022-08-23 16:12:21 +0700

    Update style

--- includes/class-pc-admin-settings.php
+++ includes/class-pc-admin-settings.php
@@ -128,34 +128,42 @@ if (!class_exists('Printcart_Admin_Settings')) {
             }
 ?>
             <div id="printcart-design">
-                <img src="<?php echo PRINTCART_PLUGIN_URL . 'assets/images/logo-printcart.svg'; ?>" class="printcart-logo" />
+                <a href="<?php echo esc_attr(PRINTCART_BACKOFFICE_URL); ?>">
+                    <img src="<?php echo esc_attr(PRINTCART_PLUGIN_URL . 'assets/images/logo-printcart.svg'); ?>" class="printcart-logo" />
+                </a>
                 <div class="printcart-box">
-                    <div class="printcart-setup-instructions">
-                        <h2><?php esc_html_e('Connect the Printcart Dashboard to your site', 'printcart-integration'); ?></h2>
-                        <div class="pc-connect-dashboard button-primary" data-url="<?php echo esc_attr($url); ?>" target="_blank"><?php esc_html_e('Connect to Dashboard', 'printcart-integration'); ?></div>
-                    </div>
-                    <div class="printcart-connected-wrap">
-                        <?php
-                        if (isset($printcart_account['unauth_token']) && $printcart_account['unauth_token']) {
-                        ?>
+                    <?php
+                    if (isset($printcart_account['unauth_token']) && $printcart_account['unauth_token']) {
+                    ?>
+                        <div class="printcart-setup-instructions">
+                            <h1><?php esc_html_e('Congratulations', 'printcart-integration'); ?></h1>
+                        </div>
+                        <div class="printcart-connected-wrap">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#008000" class="bi bi-check-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                 <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z" />
                             </svg>
-                            <span class="printcart-connected-text" style="color: #008000"><b><?php esc_html_e('The plugin has been connected to the Printcart dashboard.', 'printcart-integration'); ?>
+                            <span class="printcart-connected-text" style="color: #008000"><b><?php esc_html_e('Your website has been successfully connected to the Printcart dashboard.', 'printcart-integration'); ?>
                                 </b></span>
-                        <?php
-                        } else {
-                        ?>
+                        </div>
+                        <a href="<?php echo esc_attr(PRINTCART_BACKOFFICE_URL); ?>" class="pc-button-dashboard button-primary" target="_blank"><?php esc_html_e('Go to Dashboard', 'printcart-integration'); ?></a>
+                    <?php
+                    } else {
+                    ?>
+                        <div class="printcart-setup-instructions">
+                            <h1><?php esc_html_e('Connect the Printcart Dashboard to your site', 'printcart-integration'); ?></h1>
+                        </div>
+                        <div class="printcart-connected-wrap">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffa500" class="bi bi-x-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
                             </svg>
-                            <span class="printcart-connected-text" style="color: #ffa500"><b><?php esc_html_e('The plugin has not been connected to the Printcart dashboard!', 'printcart-integration'); ?></b></span>
-                        <?php
-                        }
-                        ?>
-                    </div>
+                            <span class="printcart-connected-text" style="color: #ffa500"><b><?php esc_html_e('Your website has failed to connect to the Printcart dashboard!', 'printcart-integration'); ?></b></span>
+                        </div>
+                        <div class="pc-connect-dashboard button-primary" data-url="<?php echo esc_attr($url); ?>"><?php esc_html_e('Connect to Dashboard', 'printcart-integration'); ?></div>
+                    <?php
+                    }
+                    ?>
                 </div>
                 <div class="printcart-box">
                     <a class="manually-key" href=""><?php esc_html_e('Manually enter an API key', 'printcart-integration'); ?></a>
@@ -194,7 +202,7 @@ if (!class_exists('Printcart_Admin_Settings')) {
                             </tbody>
                         </table>
                         <p class="submit">
-                            <button name="save" class="button-primary" type="submit" value="Save changes"><?php esc_html_e('Save changes', 'printcart-integration'); ?></button>
+                            <button name="save" class="pc-button-dashboard button-primary" type="submit" value="Save changes"><?php esc_html_e('Save changes', 'printcart-integration'); ?></button>
                             <input type="hidden" id="_action" name="_action" value="submit">
                         </p>
                     </form>
