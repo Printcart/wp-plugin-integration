4e5bd55c6be832c7493bd40c519a66637cff7295
Hiep Vu <hiepv511@gmail.com>
2022-08-26 15:15:45 +0700

    update version

--- printcart-design.php
+++ printcart-design.php
@@ -16,7 +16,7 @@ WC tested up to: 6.5.1
 PHP: >=7.0
 */
 
-define('PRINTCART_VERSION',            '1.0.0');
+define('PRINTCART_VERSION',            '1.1.0');
 define('PRINTCART_PLUGIN_URL',         plugin_dir_url(__FILE__));
 define('PRINTCART_PLUGIN_DIR',         plugin_dir_path(__FILE__));
 
