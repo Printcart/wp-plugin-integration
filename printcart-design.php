d2daa2f634af485b0792ed9c0bcf2b8b31dfddf0
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2023-03-29 09:35:23 +0700

    update version

--- printcart-design.php
+++ printcart-design.php
@@ -7,7 +7,7 @@
 Plugin Name: Printcart Integration
 Plugin URI: https://printcart.com
 Description: Create design buttons for WC products
-Version: 2.1.0
+Version: 2.2.0
 Author: Printcart Team
 Author URI: https://printcart.com
 Text Domain: printcart-integration
@@ -16,7 +16,7 @@ WC tested up to: 6.5.1
 PHP: >=7.0
 */
 
-define('PRINTCART_VERSION',            '2.1.0');
+define('PRINTCART_VERSION',            '2.2.0');
 define('PRINTCART_W2P_PLUGIN_URL',         plugin_dir_url(__FILE__));
 define('PRINTCART_W2P_PLUGIN_DIR',         plugin_dir_path(__FILE__));
 
